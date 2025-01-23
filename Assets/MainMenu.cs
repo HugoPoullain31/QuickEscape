a06ea18a0c3991a1f5ddb3b0f47bb48a543326d8
lraphael <raphael.lamarche@ynov.com>
2025-01-23 02:04:12 +0100

    PageV1+Music

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string levelToLoad;
+
+    public GameObject settingWindow;
+  public void StartGame()
+  {
+        SceneManager.LoadScene(levelToLoad);
+
+  }
+   public void SettingButton()
+  {
+    settingWindow.SetActive(true);
+    
+  }
+
+  public void CloseSettingWindow()
+  {
+    settingWindow.SetActive(false);
+  }
+   public void QuitGame()
+  {
+      Application.Quit();
+    
+  }
+}
---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using System.Linq;
+public class SettingMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+ public TMPro.TMP_Dropdown resolutionDropdown;
+    public Toggle toggleFullScreen;
+    Resolution[] resolutions;
+    public void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        Screen.fullScreen = true;
+    }
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("volume",volume);
+
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+
+    }
+}
